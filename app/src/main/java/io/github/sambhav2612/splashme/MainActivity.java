05db595e5e381abd90d78dd995ca2b2b2560c150
sambhav.github.io <sambhavjain2612@gmail.com>
2017-10-19 11:51:33 +0530

    final build. working.

---
+++ app/src/main/java/io/github/sambhav2612/splashme/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.sambhav2612.splashme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
